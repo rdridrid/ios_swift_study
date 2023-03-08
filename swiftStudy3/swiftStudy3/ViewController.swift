5da732eaa89352afe646ffe3a61389e2559c6c19
rdridrid <rdridrid@naver.com>
2023-03-08 20:38:04 +0900

    뷰 컨트롤러 기반

---
+++ swiftStudy3/swiftStudy3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftStudy3
+//
+//  Created by lee on 2023/03/08.
+// 뷰 컨트롤러 기반
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
