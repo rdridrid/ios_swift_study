d050558ae30fbda76fc5ec4e9c9f48482844a9c5
rdridrid <rdridrid@naver.com>
2023-03-20 20:56:09 +0900

    Table Layout

---
+++ TableViewController/TableViewController/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddViewController.swift
+//  TableViewController
+//
+//  Created by lee on 2023/03/20.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
