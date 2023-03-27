168528a267831a4ca2732a1fecb169b77871af23
rdridrid <rdridrid@naver.com>
2023-03-27 23:33:24 +0900

    녹음 타이머 설정

--- Audio/Audio/ViewController.swift
+++ Audio/Audio/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     var progressTimer : Timer!
     
     let timePlayerSelector:Selector = #selector(ViewController.updatePlayTime)
-    
+    let timeRecordSelector:Selector = #selector(ViewController.updateRecordTime)
     @IBOutlet var pvProgressPlay: UIProgressView!
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblEndTime: UILabel!
@@ -172,12 +172,17 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
         if(sender as AnyObject).titleLabel?.text == "record"{
             audioRecorder.record()
             (sender as AnyObject).setTitle("Stop", for: UIControl.State())
+            progressTimer = Timer.scheduledTimer(timeInterval:0.1,target: self, selector: timeRecordSelector, userInfo: nil, repeats: true)
         } else{
             audioRecorder.stop()
+            progressTimer.invalidate()
             (sender as AnyObject).setTitle("Record", for: UIControl.State())
             btnPlay.isEnabled = true
             initPlay()
         }
+    }
+    @objc func updateRecordTime(){
+        lblRecordTime.text = convertNSTimeInterval2String(audioRecorder.currentTime)
     }
 }
 
