7107e542530a5e645778140f42719cbdbbce2328
rdridrid <rdridrid@naver.com>
2023-03-31 20:33:48 +0900

    선, 원, 사각형, 호 그리기

---
+++ DrawGraphics/DrawGraphics/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  DrawGraphics
+//
+//  Created by lee on 2023/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imgView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func btnDrawLine(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size) //컨텍스트의 크기를 이미지 뷰와 같게
+        let context = UIGraphicsGetCurrentContext()! //생성한 콘텍스트 정보 연결
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.move(to: CGPoint(x: 70, y: 50)) //(0,0) 왼쪽 위
+        context.addLine(to: CGPoint(x: 270, y: 250))
+        
+        context.strokePath()
+        
+        context.setLineWidth(4.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        
+        context.move(to: CGPoint(x:170, y:200))
+        context.addLine(to: CGPoint(x: 270, y:350))
+        context.addLine(to: CGPoint(x: 70, y:350))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func btnDrawRectangle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addRect(CGRect(x: 70, y: 100, width:  200, height: 200))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func btnDrawCircle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 50, width: 200, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 200, width: 200, height: 200))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    
+    @IBAction func btnDrawArc(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.move(to: CGPoint(x: 100, y: 50))
+        context.addArc(tangent1End: CGPoint(x: 250, y: 50), tangent2End: CGPoint(x:250, y: 200), radius: CGFloat(50))
+        context.addLine(to: CGPoint(x: 250, y: 200))
+        
+        context.move(to: CGPoint(x: 100, y: 250))
+        context.addArc(tangent1End: CGPoint(x: 270, y: 250), tangent2End: CGPoint(x:100, y:400), radius: CGFloat(20))
+        context.addLine(to: CGPoint(x: 100, y: 400))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func btnDrawFill(_ sender: UIButton) {
+    }
+    
+}
+
